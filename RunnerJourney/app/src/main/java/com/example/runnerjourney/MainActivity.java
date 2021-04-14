b57a7c509b2765fd9e8a2b4269117dbc166fa533
YipHoiChun <yiphoichun29@gmail.com>
2021-04-14 11:46:47 +0800

    add two button add two page

--- RunnerJourney/app/src/main/java/com/example/runnerjourney/MainActivity.java
+++ RunnerJourney/app/src/main/java/com/example/runnerjourney/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.runnerjourney;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickRecord(View v) {
+        // go to the record journey activity
+        Intent journey = new Intent(MainActivity.this, RecordActivity.class);
+        startActivity(journey);
+    }
+
+    public void onClickView(View v) {
+        // go to the activity for displaying journeys
+        Intent view = new Intent(MainActivity.this, JourneysActivity.class);
+        startActivity(view);
+    }
+
+
 }
